f9bd85c7c7b173ab1144b10b555818757f5f6fd0
Ahmed Elsayes <mysterious.en2010@gmail.com>
2018-06-24 20:59:04 +0300

    last

--- opc-ua for water tank control.js
+++ opc-ua for water tank control.js
@@ -1,6 +1,6 @@
 // This is Javascript code to control water level in a tank
 // to make it functional, google on npm water-tank and follow the instructions to install the module
-
+// copy rights of this code returns to the creator of Water-tank module
 var Tank = require('water-tank');
 var tank1 = new Tank(10, 2.5, 1, 0.013, 0.7, 0.60, 3001);
 tank1.on('HIGH_LEVEL', function (message) {
