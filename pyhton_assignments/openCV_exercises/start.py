5b9e0715e708f78c4def21c0eddc85d6513d2e65
Ahmed Elsayes (TAU) <ahmed.elsayes@tuni.fi>
2020-02-23 17:41:53 +0200

    commit all

--- pyhton_assignments/openCV_exercises/start.py
+++ pyhton_assignments/openCV_exercises/start.py
@@ -1,3 +1,34 @@
-import cv2
+import cv2 as cv
 
-print(cv2.__version__)
+print(cv.__version__)
+
+# second_task
+img = cv.imread('nature.jpg', 0)    # 0 means reading it in grayscale 1 colored
+cv.imshow('new_img',img)
+cv.waitKey(5000)    # the number to hold image in seconds
+cv.destroyAllWindows()
+
+# third task
+cv.imwrite('messigray.png',img)
+
+# task 4
+import numpy as np
+import cv2 as cv
+img = cv.imread('nature.jpg',0)
+cv.imshow('image',img)
+k = cv.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv.destroyAllWindows()
+elif k == ord('s'): # wait for 's' key to save and exit
+    cv.imwrite('messigray.png',img)
+    cv.destroyAllWindows()
+
+
+# task 5
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+img = cv.imread('messi5.jpg',0)
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
