dba1ad6aeda6cada46482cf17d65c4e44b13dfa9
Ahmed Elsayes (TAU) <ahmed.elsayes@tuni.fi>
2020-03-09 10:39:42 +0200

    commit

--- python_assignments/fibdata/difference.py
+++ python_assignments/fibdata/difference.py
@@ -1,4 +1,3 @@
-# import numpy as np
 def subtractor(list):
     length = len(list)
     result=[]
@@ -6,13 +5,3 @@ def subtractor(list):
         ii = list[i]-list[i-1]
         result.append(ii)
     return result
-
-
-# Z = np.linspace(2, 10, num=5)
-# A = [2, 5, 7, 9, 12, 14, 16]
-# B = [2, 5, 7, 9, 2, 2, 2]
-# C = np.divide(A,B)
-# values = Z[np.array([1,4])]
-# print(values)
-# rr = subtractor(A)
-# print(rr)
