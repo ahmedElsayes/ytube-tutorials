17314bbd4dfd54c04a6a8ad967efe2708f5b1b08
Ahmed Elsayes <mysterious.en2010@gmail.com>
2020-04-02 10:40:49 +0300

    commit

---
+++ python_assignments/fibdata/droplet_detect.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2 as cv
+
+
+img1 = cv.imread('image1.jpg')
+img = cv.resize(img1,(512,512))
+cv.namedWindow('track')
+
+# threshold_var = 80
+
+def output(x):
+    print(x)
+
+# Trackbar name, the image in connection, start varying point, end point, fuction to excute
+cv.createTrackbar('thre', 'track', 0, 255, output)
+while(1):
+    threvar = cv.getTrackbarPos('thre', 'track')
+    _, th1 = cv.threshold(img, threvar, 255, cv.THRESH_BINARY)
+
+    cv.imshow('image', th1)
+    k = cv.waitKey(1)
+    if k == 27:
+        break
+    # now exploring different types of threshold
+    # or Gaussian type [Numbers: threshold, number of pixels to be in calculation, the offset constant]
+    # to get the color channels (RGB) from the trackbar
+cv.destroyAllWindows()
