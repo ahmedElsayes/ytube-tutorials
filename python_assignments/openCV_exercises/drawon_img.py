33e85e4829707e3709807a71a89adc221468d380
Ahmed Elsayes (TAU) <ahmed.elsayes@tuni.fi>
2020-04-09 20:10:58 +0300

    commit

--- python_assignments/openCV_exercises/drawon_img.py
+++ python_assignments/openCV_exercises/drawon_img.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-img = cv.imread('afganG',0)
+img = cv.imread('afganG.jpg',0)
 cv.line(img, (0,0), (255,255), (0,0,255), 5)    # start coordinate, end coordinate, color(BGR), thickness
 cv.arrowedLine(img, (255,0), (255,255), (0,0,255), 10)  # start coordinate, end coordinate, color(BGR), thickness
 cv.rectangle(img,(384,0),(510,128),(0,255,0),3)  # start coordinate, end coordinate, color(BGR), thickness
@@ -10,6 +10,6 @@ cv.ellipse(img,(256,256),(100,50),0,0,180,255,-1) # center location, lengths of 
 font = cv.FONT_HERSHEY_SIMPLEX
 cv.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv.LINE_AA) # text, location, forntformat, fontsize, color, thickness, linetype
 
-cv.imshow('afgan_girl', img)
+cv.imshow('girl', img)
 cv.waitKey(0)
 cv.destroyAllWindows()
