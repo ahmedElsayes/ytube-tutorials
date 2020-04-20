9109de71776ba9458ff552e334d8cde371b61363
Ahmed Elsayes (TAU) <ahmed.elsayes@tuni.fi>
2020-04-20 13:21:29 +0300

    c

--- python_assignments/openCV_exercises/motion_detection.py
+++ python_assignments/openCV_exercises/motion_detection.py
@@ -14,16 +14,16 @@ while cap.isOpened():
         _, thresh = cv.threshold(blur, 20, 255, cv.THRESH_BINARY)   # threshold on grayscale to tune motion detection
         dilated = cv.dilate(thresh, None, iterations=3) # TO FILTER THE UNWANTED CONTOURS INSIDE THE RESULT
         contours, _ = cv.findContours(dilated, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)    # find the contours resulted from frames substraction
-        # cv.drawContours(frame1, contours, -1, (255,0,0), 2) # draw all contours
+        cv.drawContours(frame1, contours, -1, (255,0,0), 2) # draw all contours
 
         # *********This section to adjust the tracking boundaries to a rectangle..............
-        for contour in contours:
-            (x,y,w,h) = cv.boundingRect(contour)
+        # for contour in contours:
+        #     (x,y,w,h) = cv.boundingRect(contour)
 
-            if cv.contourArea(contour) > 900:
-                cv.rectangle(frame1, (x,y), (x+w,y+h), (255,0,0), 2)
-            else:
-                continue
+        #     if cv.contourArea(contour) > 900:
+        #         cv.rectangle(frame1, (x,y), (x+w,y+h), (255,0,0), 2)
+        #     else:
+        #         continue
 
         # *********This section to adjust the tracking boundaries to a rectangle..............
 
